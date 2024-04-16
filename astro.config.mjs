6d5eb4bb9174f7213b020333c65d2759cd7ca35d
ndamulelo <nbanzyme@gmail.com>
2024-04-16 20:14:13 +0200

    update site

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://notion-cms-astro-blog.netlify.com',
   integrations: [mdx(), sitemap(), tailwind()]
 });
